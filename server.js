7a329b15d8e70b0dcd75273c67b26a09d5797e25
Neptune-PC <neptune-pc@Neptune-PCs-MacBook-Pro.local>
2024-05-09 12:52:22 +0530

    verify functionality testing code

--- server.js
+++ server.js
@@ -59,23 +59,19 @@ app.post("/send-otp", (req, res) => {
 
 // Route for verifying OTP
 app.post("/verify-otp", (req, res) => {
-    // const enteredOTP = req.body.otp;
-    // const phoneNumber = req.body.to;
-    // const storedOTP = req.cookies['otp_' + phoneNumber];
-
-    // if (!storedOTP) {
-    //     return res.status(400).json({ success: false, message: "No OTP found for the given phone number." });
-    // }
-
-    // const hashedEnteredOTP = hashOTP(enteredOTP);
-
-    // if (hashedEnteredOTP === storedOTP) {
-    //     res.clearCookie('otp_' + phoneNumber);
-    //     res.status(200).json({ success: true, message: "OTP verified successfully." });
-    // } else {
-    //     res.status(400).json({ success: false, message: "Invalid OTP. Please try again." });
-    // }
-    res.status(200).send("verify code!")
+    const enteredOTP = req.body.otp;
+    const phoneNumber = req.body.to;
+    const storedOTP = req.cookies['otp_' + phoneNumber];
+    if (!storedOTP) {
+        return res.status(400).json({ success: false, message: "No OTP found for the given phone number." });
+    }
+    const hashedEnteredOTP = hashOTP(enteredOTP);
+    if (hashedEnteredOTP === storedOTP) {
+        res.clearCookie('otp_' + phoneNumber);
+        res.status(200).json({ success: true, message: "OTP verified successfully." });
+    } else {
+        res.status(400).json({ success: false, message: "Invalid OTP. Please try again." });
+    }
 });
 
 // Error handling middleware
